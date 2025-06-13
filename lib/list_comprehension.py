7419c068e1b29f3b9c4ab1bc3af6d9c12a11afe2
iqra <abdikarimiqra@gmail.com>
2025-06-13 10:08:09 +0300

    assign

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+ return [num for num in num_list if num % 2==0]
+num_list = range(1,10)
+print (num_list)
+
+ 
+  
 
 def make_exclamation(sentence_list):
-    pass
+ return [sentence + '!' for sentence in sentence_list]
+sentence_list = ["Hello", "I'm doing great", "Python is fun"]
+
+
+print(sentence_list)
+    
+    
